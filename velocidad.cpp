5d329f83df32b2f732572351bc8165ae86053504
Amezande <63935105+Amezande@users.noreply.github.com>
2022-02-26 18:49:29 -0600

    Add files via upload
    c ++

---
+++ velocidad.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+   int main()
+   {
+   	double t,d,velocidad=d/t;
+   	
+   	printf("introduzca el tiempo");
+   	scanf("%lf",&t);
+   	printf("introduzca la distancia");
+   	scanf("%lf",&d);
+   	
+   	velocidad=d/t;
+   	printf("la velocidad es de:%lf",velocidad);
+   	
+   	return 0;
+   }
