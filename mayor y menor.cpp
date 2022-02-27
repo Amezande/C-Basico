ce022c390e45bba1ccd74e99b312fb5ff40ecb24
Amezande <63935105+Amezande@users.noreply.github.com>
2022-02-26 18:47:43 -0600

    Add files via upload
    c ++

---
+++ mayor y menor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+#include <iomanip>
+#include <conio.h>
+
+ using namespace std;
+  int main(){
+    int a,b,c;
+    
+    cout<<"introduzca el numero ";
+    cin>>a;
+    cout<<"introduzca el numero ";
+    cin>>b;
+    cout<<"introduzca el numero ";
+    cin>>c;
+    
+    if(a>b,a>c){
+    	cout<<"el numero mayor es"<<a<<endl<<endl;
+	}
+	else if (b>a,b>c){
+		cout<<"el numero mayor es"<<b<<endl<<endl; 
+	}
+	else if (c>a,c>b){
+		cout<<"el numero mayor es"<<c<<endl<<endl;
+	}
+	system ("pause");
+   }
